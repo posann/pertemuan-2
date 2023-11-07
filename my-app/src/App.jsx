dd8e4add2bffd82e285d8dd64717848cb33eacfa
Ahmad Fauzan <57349734+afzn9x@users.noreply.github.com>
2023-11-07 10:04:43 +0700

    update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 import Navbar from './components/navbar'
 
 const App = () => {
   return(
     <>
       <Navbar />
+      <Hero />
+      <Footer />
     </>
   )
 }
